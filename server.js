1aa871714c61aadfdf4ca5843dd8982106502652
Tomas <tomas.pinto94mmil@gmail.com>
2022-08-13 16:54:25 -0500

    proyect setup

---
+++ server.js
@@ -0,0 +1,50 @@
+//TODO setup:
+//jsonwebtoken
+//pg
+
+const express = require("express");
+const exphbs = require("express-handlebars");
+const expressFileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const secretKey = "Shhhh";
+
+const PORT = 3000;
+
+const app = express();
+
+app.listen(PORT, () =>
+  console.log("Servidor encendido, escuchando puerto: ", PORT)
+);
+
+//Integrar body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Directorio publico
+app.use(express.static(__dirname + "/public"));
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
+
+//Middleware
+app.use(
+  expressFileUpload({
+    limits: 5000000,
+    abortOnLimit: true,
+    responseOnLimit: "El tamaño de la imagen supera el limite permitido",
+  })
+);
+
+//motor de plantillas
+app.engine(
+  "handlebars",
+  exphbs.engine({
+    defaultLayout: "main",
+    layoutsDir: `${__dirname}/views/mainLayout`,
+  })
+);
+app.set("view engine", "handlebars");
+
+//Rutas
+app.get("/", async (req, res) => {
+  res.render("index"); //aqui como segundo argumento {helper}
+});
